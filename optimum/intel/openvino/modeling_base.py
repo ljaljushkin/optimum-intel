cd9f0a07cd431d3e9e2389170556826cac6721fd
eaidova <ekaterina.aidova@intel.com>
2023-07-19 11:41:07 +0400

    fixes for seq2seq

--- optimum/intel/openvino/modeling_base.py
+++ optimum/intel/openvino/modeling_base.py
@@ -17,6 +17,7 @@ import os
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Dict, Optional, Union
+import time
 
 import openvino
 from huggingface_hub import hf_hub_download
@@ -130,8 +131,10 @@ class OVBaseModel(PreTrainedModel):
         if isinstance(file_name, str):
             file_name = Path(file_name)
         bin_file_name = file_name.with_suffix(".bin") if file_name.suffix == ".xml" else None
-
+        s = time.perf_counter()
         model = core.read_model(file_name, bin_file_name) if not file_name.suffix == ".onnx" else mo.convert_model(file_name)
+        e = time.perf_counter()
+        print(f"Read model took {e - s}s")
         if file_name.suffix == ".onnx":
             model = fix_op_names_duplicates(model)  # should be called during model conversion to IR
 
@@ -277,8 +280,8 @@ class OVBaseModel(PreTrainedModel):
         )
 
         onnx_config = onnx_config_class(model.config)
-        #save_dir = #TemporaryDirectory()
-        save_dir_path = Path("./model")
+        save_dir = TemporaryDirectory()
+        save_dir_path = Path(save_dir.name)
 
         # Export the model to the ONNX format
         export(
