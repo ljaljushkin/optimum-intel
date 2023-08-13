bced6860cab7fe4d95adf60cc84fbffc9cc41849
eaidova <ekaterina.aidova@intel.com>
2023-08-13 17:36:24 +0400

    rebase with fixes

--- optimum/intel/openvino/modeling_base.py
+++ optimum/intel/openvino/modeling_base.py
@@ -28,8 +28,9 @@ from transformers.file_utils import add_start_docstrings
 from optimum.exporters import TasksManager
 from optimum.modeling_base import OptimizedModel
 
+from ...exporters.openvino import export
+from ...exporters.openvino.utils import is_torch_model
 from ..utils.import_utils import is_transformers_version
-from .export import export, is_torch_model
 from .utils import ONNX_WEIGHTS_NAME, OV_XML_FILE_NAME
 
 
@@ -128,10 +129,7 @@ class OVBaseModel(PreTrainedModel):
 
         if isinstance(file_name, str):
             file_name = Path(file_name)
-        bin_file_name = file_name.with_suffix(".bin") if file_name.suffix == ".xml" else None
-        model = (
-            core.read_model(file_name, bin_file_name) if not file_name.suffix == ".onnx" else convert_model(file_name)
-        )
+        model = core.read_model(file_name) if not file_name.suffix == ".onnx" else convert_model(file_name)
         if file_name.suffix == ".onnx":
             model = fix_op_names_duplicates(model)  # should be called during model conversion to IR
 
@@ -198,8 +196,9 @@ class OVBaseModel(PreTrainedModel):
             model_save_dir = model_id
         # Download the model from the hub
         else:
-            model_file_names = [file_name] if from_onnx else []
+            model_file_names = [file_name]
             # If not ONNX then OpenVINO IR
+
             if not from_onnx:
                 model_file_names.append(file_name.replace(".xml", ".bin"))
             file_names = []
